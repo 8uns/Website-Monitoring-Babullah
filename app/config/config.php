232e784ac270281d8572043cf49a1096b4ab51e1
8uns <bunsdev08@gmail.com>
2022-11-02 16:20:04 +0900

    update define url

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,15 @@
 <?php
 // define('BASEURL', 'http://bdev.local/upbumonitoring/public/');
-define('BASEURL', 'http://192.168.1.21/upbumonitoring/public/');
-// define('BASEURL', 'https://simpelbabullah.online/public/');
-// define('BASEURL', 'https://simpelbabullah.online/');
+// define('BASEURL', 'http://192.168.1.21/upbumonitoring/public/');
+// // define('BASEURL', 'https://simpelbabullah.online/public/');
+// // define('BASEURL', 'https://simpelbabullah.online/');
+
+define('BASEURL', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'
+    ? "https"
+    : "http")
+    . "://$_SERVER[HTTP_HOST]" . dirname($_SERVER['SCRIPT_NAME'])
+    . (!str_ends_with(dirname($_SERVER['SCRIPT_NAME']), "/") ? "/" : ""));
+
 
 // // DB
 define('DB_HOST', 'localhost');
